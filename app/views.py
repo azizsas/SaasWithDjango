fda7ac0016da7b15d21034d1098cf684dd1a694f
GHILENISABEUR <ghilanisaber191@gmail.com>
2025-03-08 01:53:53 -0500

    hata chy

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+import pathlib
 # Create your views here.
 def home(request,*args,**kwargs):
     my_title = "Hello there..."
-    return render(request, 'index.html', {"title":my_title})
+    my_context = {
+        "title":my_title
+    }
+    html_template = "index.html"
+    return render(request, 'index.html', my_context)
