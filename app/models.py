c5a41cf048711809d748c1d1ac16521709cdae39
GHILENISABEUR <ghilanisaber191@gmail.com>
2025-03-08 20:18:48 -0500

    countting things playing with database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+"""
 # Create your models here.
+class Visit(models.Model):
+     #db table
+     #id primary key auto increment ,its hidden 
+    path=models.TextField()
+    timestamp=models.DateTimeField(auto_now_add=True)
+"""
+class PageVisit(models.Model):
+     #db table
+     #id primary key auto increment ,its hidden 
+    path=models.TextField(blank=True,null=True)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.shortcuts import render
 import pathlib
+from . models import PageVisit
 # Create your views here.
+this_dir=pathlib.Path(__file__).resolve().parent
 def home(request,*args,**kwargs):
+    qs=PageVisit.objects.all()
+    page_qs= PageVisit.objects.filter(path=request.path)
+
     my_title = "Hello there..."
     my_context = {
-        "title":my_title
+        "title": my_title,
+        "query_set_count": page_qs.count(),
+        "total_visit_count": qs.count(),
+        "percentage": (page_qs.count()/qs.count())*100
     }
+
+    path = request.path
+    print('path',path)
     html_template = "index.html"
-    return render(request, 'index.html', my_context)
+    PageVisit.objects.create(path=request.path)
+    return render(request,html_template,my_context)
